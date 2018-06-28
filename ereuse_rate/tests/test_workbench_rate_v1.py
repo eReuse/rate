7bd03f8e18dfe0f5ab0815278e8e82e9760f527c
JNadeu <jnadeu@gmail.com>
2018-06-28 12:38:27 +0200

    init tests v1

---
+++ ereuse_rate/tests/test_workbench_rate_v1.py
@@ -0,0 +1,68 @@
+import pytest
+
+
+# RAM MODULE DEVICE TEST
+
+def test_ram_size_is_0 ():
+    """
+    Test where doing input RamModule.size = 0
+    :return:
+    """
+
+def test_ram_speed_is_null():
+    """
+    Test where RamModule.speed is NULL
+    :return:
+    """
+
+def test_no_ram_modules():
+    """
+    Test where len(Collection[RamModule]) == 0
+    :return:
+    """
+
+# DATA STORAGE DEVICE TEST
+
+def test_data_storage_size_is_null():
+    """
+    Test where doing input DataStorage.size = NULL
+    :return:
+    """
+
+def test_data_storage_read_speed_is_0():
+    """
+    Test where BenchmarkDataStorage.read_speed = 0
+    :return:
+    """
+
+def test_data_storage_write_speed_is_0():
+    """
+    Test where BenchmarkDataStorage.write_speed = 0
+    :return:
+    """
+
+def test_no_data_storage():
+    """
+    Test with 0 data storage devices
+    :return:
+    """
+
+# PROCESSOR DEVICE TEST
+
+def test_processor_with_null_cores ():
+    """
+    Test with processor device have null number of cores
+    :return:
+    """
+
+def test_processor_with_null_speed():
+    """
+    Test with processor device have null speed value
+    :return:
+    """
+
+def test_no_processor():
+    """
+    Test with any processor device
+    :return:
+    """
